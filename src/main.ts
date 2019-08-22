9bf9d0bbcc907e82a960e7d9b12b7a0b05cdad4c
Cole Bemis <colebemis@github.com>
2019-08-22 08:37:41 -0700

    Place icons in the center of the current viewport

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ figma.showUI(__html__, { width: 300, height: 400 })
 figma.ui.onmessage = message => {
   const icon = figma.createNodeFromSvg(icons[message.type].toSvg())
   icon.name = message.type
+  icon.x = figma.viewport.center.x
+  icon.y = figma.viewport.center.y
   figma.currentPage.selection = [icon]
-  figma.viewport.scrollAndZoomIntoView([icon])
 }
